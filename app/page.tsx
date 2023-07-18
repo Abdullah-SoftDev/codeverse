8874b0ad6ca0dbe1715b255378c8b9f09173f5ac
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-19 02:18:37 +0530

    Built Feed Component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-'use client'
-import { auth } from "@/firebase/firebaseConfig";
-import { useAuthState } from "react-firebase-hooks/auth";
+import Feed from "@/components/Feed";
 
 export default function Home() {
-  const [user] = useAuthState(auth);
-  console.log(user)
   return (
-    <p className="text-xl text-center bg-red-500 text-black p-5">
-      Lets start building.🔥
-    </p>
+    <Feed />
   )
 }
